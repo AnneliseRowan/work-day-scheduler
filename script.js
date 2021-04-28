1cc7b0aa5fa82251c326e6d27be6585353318609
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-04-28 12:07:31 -0400

    got the information to stay during a refresh!

--- script.js
+++ script.js
@@ -3,56 +3,63 @@ let hourNow = moment().format("h");
 let current24 = Number(moment().format("HH")); 
 let hourInterval = Number(hourNow); 
 
-const hr9 = $("#hour1").text();
-const hr10 = $("#hour2").text(); 
-const hr11 = $("#hour3").text();
-const hr12 = $("#hour4").text(); 
-const hr13 = $("#hour5").text();
-const hr14 = $("#hour6").text(); 
-const hr15 = $("#hour7").text(); 
-const hr16 = $("#hour8").text(); 
-const hr17 = $("#hour9").text();  
+const hr9 = $("#hour9").text();
+const hr10 = $("#hour10").text(); 
+const hr11 = $("#hour11").text();
+const hr12 = $("#hour12").text(); 
+const hr13 = $("#hour13").text();
+const hr14 = $("#hour14").text(); 
+const hr15 = $("#hour15").text(); 
+const hr16 = $("#hour16").text(); 
+const hr17 = $("#hour17").text();  
 
 
 $("#currentDay").text(timeNow); 
 
+/*
+if ($("#currentDay").text(timeNow) !== $("#currentDay").text(timeNow)) {
+    reset(); 
+}
+*/
+
+$("#text9").val(localStorage.getItem(JSON.stringify(hr9))); 
+$("#text10").val(localStorage.getItem(JSON.stringify(hr10)));
+$("#text11").val(localStorage.getItem(JSON.stringify(hr11)));
+$("#text12").val(localStorage.getItem(JSON.stringify(hr12)));
+$("#text13").val(localStorage.getItem(JSON.stringify(hr13)));
+$("#text14").val(localStorage.getItem(JSON.stringify(hr14)));
+$("#text15").val(localStorage.getItem(JSON.stringify(hr15)));
+$("#text16").val(localStorage.getItem(JSON.stringify(hr16)));
+$("#text17").val(localStorage.getItem(JSON.stringify(hr17)));
+
+
 
 $("#btn1").click(function() {
-    preventDefault();
-    const hr9Str = JSON.stringify(hr9);
-    localStorage.setItem(hr9Str, $("#text1").val())
+    localStorage.setItem(hr9, $("#text1").val());
 })
 $("#btn2").click(function() {
-    const hr10Str = JSON.stringify(hr10);
-    localStorage.setItem(hr10Str, $("#text2").val())
+    localStorage.setItem(hr10, $("#text2").val());;
 })
 $("#btn3").click(function() {
-    const hr3Str = JSON.stringify(hr11);
-    localStorage.setItem(hr3Str, $("#text3").val())
+    localStorage.setItem(hr11, $("#text3").val());
 })
 $("#btn4").click(function() {
-    const hr12Str = JSON.stringify(hr12);
-    localStorage.setItem(hr12Str, $("#text4").val())
+    localStorage.setItem(hr12, $("#text4").val());
 })
 $("#btn5").click(function() {
-    const hr13Str = JSON.stringify(hr13);
-    localStorage.setItem(hr13Str, $("#text5").val())
+    localStorage.setItem(hr13, $("#text5").val());
 })
 $("#btn6").click(function() {
-    const hr14Str = JSON.stringify(hr14);
-    localStorage.setItem(hr14Str, $("#text6").val())
+    localStorage.setItem(hr14, $("#text6").val());
 })
 $("#btn7").click(function() {
-    const hr15Str = JSON.stringify(hr15);
-    localStorage.setItem(hr15Str, $("#text7").val())
+    localStorage.setItem(hr15, $("#text7").val());
 })
 $("#btn8").click(function() {
-    const hr16Str = JSON.stringify(hr16);
-    localStorage.setItem(hr16Str, $("#text8").val())
+    localStorage.setItem(hr16, $("#text8").val());
 })
 $("#btn9").click(function() {
-    const hr17tr = JSON.stringify(hr17);
-    localStorage.setItem(hr17tr, $("#text9").val())
+    localStorage.setItem(hr17, $("#text9").val());
 })
 
 
@@ -60,84 +67,89 @@ hourColor();
 
 function hourColor() {
     if(Number(hr9) <= current24) {
-        $("#text1").addClass("past")
-    }
-    if(Number(hr9) > current24) {
-        $("#text1").addClass("future")
-    }
-    if(Number(hr9) === current24) {
-        $("#text1").addClass("present")
-    }
-    if(Number(hr10) <= current24) {
-        $("#text2").addClass("past")
-    }
-    if(Number(hr10) > current24) {
-        $("#text2").addClass("future")
-    }
-    if(Number(hr10) === current24) {
-        $("#text2").addClass("present")
-    }
-    if(Number(hr11) <= current24) {
-        $("#text3").addClass("past")
-    }
-    if(Number(hr11) > current24) {
-        $("#text3").addClass("future")
-    }
-    if(Number(hr11) === current24) {
-        $("#text3").addClass("present")
-    }
-    if(Number(hr12) <= current24) {
-        $("#text4").addClass("past")
-    }
-    if(Number(hr12) > current24) {
-        $("#text4").addClass("future")
-    }
-    if(Number(hr12) === current24) {
-        $("#text4").addClass("present")
-    }
-    if(Number(hr13) <= current24) {
-        $("#text5").addClass("past")
-    }
-    if(Number(hr13) > current24) {
-        $("#text5").addClass("future")
-    }
-    if(Number(hr13) === current24) {
-        $("#text5").addClass("present")
-    }
-    if(Number(hr14) <= current24) {
-        $("#text6").addClass("past")
-    }
-    if(Number(hr14) > current24) {
-        $("#text6").addClass("future")
-    }
-    if(Number(hr14) === current24) {
-        $("#text6").addClass("present")
-    }
-    if(Number(hr15) <= current24) {
-        $("#text7").addClass("past")
-    }
-    if(Number(hr15) > current24) {
-        $("#text7").addClass("future")
-    }
-    if(Number(hr15) === current24) {
-        $("#text7").addClass("present")
-    }
-    if(Number(hr16) <= current24) {
-        $("#text8").addClass("past")
-    }
-    if(Number(hr16) > current24) {
-        $("#text8").addClass("future")
-    }
-    if(Number(hr16) === current24) {
-        $("#text8").addClass("present")
-    }
-    if(Number(hr17) <= current24) {
         $("#text9").addClass("past")
     }
-    if(Number(hr17) > current24) {
+    if(Number(hr9) > current24) {
         $("#text9").addClass("future")
     }
-    if(Number(hr17) === current24) {
+    if(Number(hr9) === current24) {
         $("#text9").addClass("present")
     }
+    if(Number(hr10) <= current24) {
+        $("#text10").addClass("past")
+    }
+    if(Number(hr10) > current24) {
+        $("#text10").addClass("future")
+    }
+    if(Number(hr10) === current24) {
+        $("#text10").addClass("present")
+    }
+    if(Number(hr11) <= current24) {
+        $("#text11").addClass("past")
+    }
+    if(Number(hr11) > current24) {
+        $("#text11").addClass("future")
+    }
+    if(Number(hr11) === current24) {
+        $("#text11").addClass("present")
+    }
+    if(Number(hr12) <= current24) {
+        $("#text12").addClass("past")
+    }
+    if(Number(hr12) > current24) {
+        $("#text12").addClass("future")
+    }
+    if(Number(hr12) === current24) {
+        $("#text12").addClass("present")
+    }
+    if(Number(hr13) <= current24) {
+        $("#text13").addClass("past")
+    }
+    if(Number(hr13) > current24) {
+        $("#text13").addClass("future")
+    }
+    if(Number(hr13) === current24) {
+        $("#text13").addClass("present")
+    }
+    if(Number(hr14) <= current24) {
+        $("#text14").addClass("past")
+    }
+    if(Number(hr14) > current24) {
+        $("#text14").addClass("future")
+    }
+    if(Number(hr14) === current24) {
+        $("#text14").addClass("present")
+    }
+    if(Number(hr15) <= current24) {
+        $("#text15").addClass("past")
+    }
+    if(Number(hr15) > current24) {
+        $("#text15").addClass("future")
+    }
+    if(Number(hr15) === current24) {
+        $("#text15").addClass("present")
+    }
+    if(Number(hr16) <= current24) {
+        $("#text16").addClass("past")
+    }
+    if(Number(hr16) > current24) {
+        $("#text16").addClass("future")
+    }
+    if(Number(hr16) === current24) {
+        $("#text16").addClass("present")
+    }
+    if(Number(hr17) <= current24) {
+        $("#text17").addClass("past")
+    }
+    if(Number(hr17) > current24) {
+        $("#text17").addClass("future")
+    }
+    if(Number(hr17) === current24) {
+        $("#text17").addClass("present")
+    }
 }
+
+var addHr = function() {
+    return $(".time").text() + "H"; 
+}
+
